106998aa26ae6d502c9f3866910c921c11bcf102
Maciej Kubas <maciekkubas@gmail.com>
2018-06-20 00:19:37 +0200

    task-completed

---
+++ Zadanie 19.1 Tworzymy akcje/actions.js
@@ -0,0 +1,48 @@
+import uuid from uuid;
+
+const ADD_COMMENT = 'ADD_COMMENT';
+const EDIT_COMMENT = 'EDIT_COMMENT';
+const REMOVE_COMMENT = 'REMOVE_COMMENT';
+const THUMB_UP_COMMENT = 'THUMB_UP_COMMENT';
+const THUMB_DOWN_COMMENT = 'THUMB_DOWN_COMMENT';
+ 
+
+const addComment = text => {
+    return {
+        type: ADD_COMMENT,
+        text,
+        id: uuid.v4(),
+        votes: 0
+    }
+};
+
+const editComment = (text, id) => {
+    return {
+        type: EDIT_COMMENT,
+        text,
+        id
+    }
+};
+
+const removeComment = id => {
+    return {
+        type: REMOVE_COMMENT,
+        id
+    }
+};
+
+const thumbUpComment = (votes, id) => {
+    return {
+        type: THUMB_UP_COMMENT,
+        votes: votes + 1,
+        id
+    }
+};
+
+const THUMB_DOWN_COMMENT = (votes, id) => {
+    return {
+        type: THUMB_DOWN_COMMENT,
+        votes: votes - 1,
+        id
+    }
+};
